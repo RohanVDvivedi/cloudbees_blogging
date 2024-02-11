d0b1f4bb40f1b6aa6c7551af038e7ceb495a239e
RohanVDvivedi <rohandvivedi@gmail.com>
2024-02-11 20:45:05 +0530

    1st request sent

--- test_client/test_client.go
+++ test_client/test_client.go
@@ -5,7 +5,9 @@ import (
 )
 
 import (
+	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 import (
@@ -15,11 +17,22 @@ import (
 var port int32 = 6959
 
 func main() {
-	var opts []grpc.DialOption
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), opts...)
 	if err != nil {
-		fmt.Println("Could not connect to client");
+		fmt.Println("Could not connect to client ", err);
+		return
 	}
 	defer conn.Close()
 	client := pb.NewBloggingServiceClient(conn)
+
+	rq1 := pb.CreateParams{
+		Title: "T1",
+		Content: "C1",
+		Author: "A1",
+		PublicationDate: "P1",
+		Tags: []string{"Tg1", "Tg2"},
+	}
+	rp1, _ := client.Create(context.Background(), &rq1)
+	fmt.Println(rq1, " -> ", rp1);
 }
