a64ec656dfa1e30ef81fff3b28e0a27c59ece47f
RohanVDvivedi <rohandvivedi@gmail.com>
2024-02-11 20:12:00 +0530

    test client compiles

--- test_client/test_client.go
+++ test_client/test_client.go
@@ -1,15 +1,25 @@
-package test_client
+package main
 
 import (
 	"fmt"
 )
 
+import (
+	"google.golang.org/grpc"
+)
+
+import (
+	"cloudbees_blogging/pb"
+)
+
+var port int32 = 6959
+
 func main() {
 	var opts []grpc.DialOption
-	conn, err := grpc.Dial(*serverAddr, opts...)
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), opts...)
 	if err != nil {
 		fmt.Println("Could not connect to client");
 	}
 	defer conn.Close()
-	client := pb.NewRouteGuideClient(conn)
+	client := pb.NewBloggingServiceClient(conn)
 }
